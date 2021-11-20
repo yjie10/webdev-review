34ee498dcb4e3423be1966ea2a9f73ab6c4d7d9d
yjie10 <yy.jie28@gmail.com>
2021-11-19 22:00:42 -0800

    adv-js ES2020 - str.replaceAll()

---
+++ JavaScript/es2021/replaceAll.js
@@ -0,0 +1,5 @@
+// not many new features in ES2021; short notes
+
+// string.replaceAll(wordToBeReplaced, wordToReplaceWith);
+
+// JS have .replace() before, but the problem is that it only replaces the first instance. 
