627af8308b0261c69f07fb11423269d7790b99d5
yjie10 <yy.jie28@gmail.com>
2022-02-22 19:02:08 -0800

    array method - find

--- JavaScript/ES5+ES6/ArrayMethods/script.js
+++ JavaScript/ES5+ES6/ArrayMethods/script.js
@@ -16,4 +16,10 @@ const newIncludesArray2ndParam = myArray.includes(2, 3) // 2nd param (optional) 
 // everytime an object is instantiated, JS is storing that object as a brand new reference in memory
 // JS is not comparing the value, it is comparing if the reference in the memory is the same
 
-// 
+// find - trying to find an element that correctly returns true based on the conditional that we have
+// very similiar to filter
+const newFindArray = myArray.find(el => el === 5); // gives back the first val in the array that return true; loop will stop there
+myArray.find(el => el > 4); // will only return 5
+
+const peopleArray = [{ id: 1 }, { id: 8 }, { id: 6 }];
+peopleArray.find(person => person.id === 4);
